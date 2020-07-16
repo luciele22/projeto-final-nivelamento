4ff0c702bdad8ab3cdb44ec3defa497468c3997a
Luciele <luciellerodrigues22@gmail.com>
2020-07-16 11:14:12 -0300

    exercicios nivelamento

---
+++ javaScript.js
@@ -0,0 +1,78 @@
+//Variáveis pode se modificar, não é fixo
+//constantes ao contrário da variavel é fixo, não varia
+var professor
+
+
+//Comandos de decisão  comandos usados para ajudar nas tomadas de decisao 
+if, else, return 
+
+
+//Loops
+//São laços de repetições , são uteis se você quiser executar o mesmo código uma e outra
+//vez, cada vez com um valor diferente
+//Exemplos: For, for/in,for/of,while
+
+//Funções  serve para executar uma tarefa,
+//elas podem ser com ou sem parâmetro e com ou sem retorno
+alert(oi); void return
+
+//Escopos- os escopos podem ser: globais(qualquer um acessa) ou locais 
+//(somente o local pode acessar)
+
+//Inputs e outputs
+//Input- entrada de dados Prompt,input
+//Output- saída de dados- alert,console.log
+
+
+
+//Array é uma lista, tipo de variável que armazena mais de um dado é representado por [ ],[ ]
+[teclado],[violão];
+
+//Objetos assim como o array é usado para criar listas, porem no objeto 
+//podemos criar varias listas com varios objetos(coisas) distintas 
+//é representado por { }
+//{nome:"Luciele"idade:33}
+
+
+//camelCase- começa com a primeira letra da primeira palavra
+// minúscula e primeira letra da segunda palavra maiúscula
+//PascalCase- as duas iniciam com letras maiúsculas
+//kibab-case -iniciam por letras minusculas separadas por um hífen
+//snake_case -iniciam com letras minusculas separadas por _
+
+
+
+
+//Diferçaa entre :
+//caminho absoluto - são caminhos fixos
+cabeçalho
+//caminho relativo- são caminhos que se modificam
+Array, document.write
+
+
+//Diferença entre operadores
+//&& operador lógico
+//|| significa ou
+// -- sinal de igual
+//!- sinal de diferente
+
+// O que é EPER Entender, Planejar, Executar e Revisar
+//regrinha básica para ajudar na elaboração de um bom código
+
+//Pilha  o primeiro que entra é o último que sai
+//fila- o primeiro que entra é o primeiro que sai
+
+
+// html,javaScript e css
+//html é uma linguagem estatica,não se altera, construido por tags, 
+//representado por .html(ustentação)
+
+//ccs estática e estética da tela, visual(beleza)
+
+//JavaScript é uma linguagem dinâmica´faz alteração em ccs,html e 
+//javascript (vida)
+
+
+
+
+
